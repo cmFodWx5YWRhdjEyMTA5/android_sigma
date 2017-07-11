c5c932c812ea9cd1132c0e9ab463d36dc2d9282d
Lucgu <lucgu.qolfiera.m@mail.ugm.ac.id>
2017-07-11 22:21:55 +0700

    Add Local Session

--- app/src/main/java/com/sigma/prouds/network/RetrofitClient.java
+++ app/src/main/java/com/sigma/prouds/network/RetrofitClient.java
@@ -1,5 +1,11 @@
 package com.sigma.prouds.network;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -11,13 +17,28 @@ public class RetrofitClient
 {
     private static Retrofit retrofit = null;
 
+
+
     public static Retrofit getClient(String baseUrl) {
+
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(120, TimeUnit.SECONDS)
+                .connectTimeout(120, TimeUnit.SECONDS)
+                .build();
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
         return retrofit;
     }
+
+
 }
---
+++ app/src/main/java/com/sigma/prouds/session/SessionManager.java
@@ -0,0 +1,42 @@
+package com.sigma.prouds.session;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by 1414 on 7/11/2017.
+ */
+
+public class SessionManager
+{
+    private static final String PREF_NAME = "prouds"; // preference filename
+    private static final int MODE_PRIVATE = 0; // preference mode operation
+
+    private static final String LOGIN = "login";
+
+    private SharedPreferences pref; // use to access preference
+    private SharedPreferences.Editor editor; // use to edit preference
+
+    public SessionManager(Context context)
+    {
+        pref = context.getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+        editor = pref.edit();
+    }
+
+    public void clearSession() // clear all value in preference
+    {
+        editor.clear();
+        editor.commit();
+    }
+
+    public boolean isLogin() // check login info
+    {
+        return pref.getBoolean(LOGIN, false); // default value = false, means no one has login
+    }
+
+    public void setLogin(boolean isLogin) // set login info when user already login
+    {
+        editor.putBoolean(LOGIN, isLogin);
+        editor.commit();
+    }
+}
