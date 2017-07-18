1acd41fb14344ed938d7b49f63282aef87d93efa
MariaGbrl <maria.gabriel.okta@gmail.com>
2017-07-19 01:18:36 +0700

    project details drawer, overview

---
+++ app/src/main/java/com/sigma/prouds/fragment/IssuesFragment.java
@@ -0,0 +1,22 @@
+package com.sigma.prouds.fragment;
+
+import android.view.View;
+
+import com.sigma.prouds.R;
+import com.sigma.prouds.base.BaseFragment;
+
+/**
+ * Created by goy on 7/15/2017.
+ */
+
+public class IssuesFragment extends BaseFragment {
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_activity;
+    }
+
+    @Override
+    protected void workingSpace(View view) {
+
+    }
+}
