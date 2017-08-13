844b1c996ed8f5c8c9d6cbf033dd9e8892e41c23
lucgudev <lucgudev@gmail.com>
2017-08-13 15:24:26 +0700

    Circular Progress Bar

---
+++ app/src/main/java/com/sigma/prouds/model/PerformanceSendModel.java
@@ -0,0 +1,27 @@
+package com.sigma.prouds.model;
+
+/**
+ * Created by lucgu.qolfiera on 8/13/2017.
+ */
+
+public class PerformanceSendModel
+{
+    private String bulan;
+    private String tahun;
+
+    public String getBulan() {
+        return bulan;
+    }
+
+    public void setBulan(String bulan) {
+        this.bulan = bulan;
+    }
+
+    public String getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(String tahun) {
+        this.tahun = tahun;
+    }
+}
