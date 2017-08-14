caf9499d8daa1b0ba101cefe8c928ec110fe131a
MariaGbrl <maria.gabriel.okta@gmail.com>
2017-08-14 20:52:19 +0700

    report issue form

---
+++ app/src/main/java/com/sigma/prouds/FormReportIssueActivity.java
@@ -0,0 +1,13 @@
+package com.sigma.prouds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormReportIssueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_report_issue);
+    }
+}
