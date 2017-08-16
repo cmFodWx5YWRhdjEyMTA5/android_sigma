cffa59618f1c623e67deca2af552722544f8c85f
lucgudev <lucgudev@gmail.com>
2017-08-16 21:36:53 +0700

    Timesheet list

--- app/src/main/java/com/sigma/prouds/holder/TimesheetHolder.java
+++ app/src/main/java/com/sigma/prouds/holder/TimesheetHolder.java
@@ -3,7 +3,9 @@ package com.sigma.prouds.holder;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
+import com.sigma.prouds.R;
 import com.sigma.prouds.model.UserActivityTimesheetModel;
 import com.sigma.prouds.network.response.UserProjectTimesheetResponse;
 
@@ -14,15 +16,27 @@ import com.sigma.prouds.network.response.UserProjectTimesheetResponse;
 public class TimesheetHolder extends RecyclerView.ViewHolder
 {
     private Context context;
+    private TextView tvTsTime;
+    private TextView tvTsSubmit;
+    private TextView tvTsSubject;
+    private TextView tvDesc;
 
     public TimesheetHolder(Context context, View itemView)
     {
         super(itemView);
         this.context = context;
+        tvTsTime = (TextView) itemView.findViewById(R.id.tv_ts_time);
+        tvTsSubmit = (TextView) itemView.findViewById(R.id.tv_ts_submit);
+        tvTsSubject = (TextView) itemView.findViewById(R.id.tv_ts_errormsg);
+        tvDesc = (TextView) itemView.findViewById(R.id.tv_act_desc);
+
     }
 
     public void bind(UserActivityTimesheetModel model)
     {
-
+        tvTsTime.setText(model.getTsDate());
+        tvTsSubject.setText(model.getSubject());
+        tvTsSubmit.setText("Submitted " + model.getHourTotal() + " hour work for " + model.getWbsName() + " in" + model.getProjectName());
+        tvDesc.setText(model.getMessage());
     }
 }
