f3b1613a12e5a394c40ac9896cb49f694bb8d95d
lucgudev <lucgudev@gmail.com>
2017-08-29 22:48:03 +0700

    Wbs Name null di task

--- app/src/main/java/com/sigma/prouds/ProfileSettingActivity.java
+++ app/src/main/java/com/sigma/prouds/ProfileSettingActivity.java
@@ -1,8 +1,14 @@
 package com.sigma.prouds;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +20,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.sigma.prouds.base.BaseActivity;
 import com.sigma.prouds.model.EditProfileSendModel;
 import com.sigma.prouds.network.ApiService;
@@ -23,6 +30,12 @@ import com.sigma.prouds.network.response.MyActivityResponse;
 import com.sigma.prouds.network.response.ProjectResponse;
 import com.sigma.prouds.network.response.UserdataResponse;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -38,8 +51,14 @@ public class ProfileSettingActivity extends BaseActivity {
     private ProgressDialog dialog;
 
     private EditText etUserId, etRole, etFullName, etEmail, etPhone, etAddress;
+    private CircleImageView ivDp;
 
     private ApiService service;
+
+    public static final int ACTION_REQUEST_CAMERA = 1;
+
+    private String mCurrentPhotoPath;
+    private String imagePath;
 
     @Override
     protected int getLayout() {
@@ -49,6 +68,11 @@ public class ProfileSettingActivity extends BaseActivity {
     @Override
     protected void workingSpace()
     {
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
         assignXML();
         app = (ProudsApplication) getApplicationContext();
         service = ApiUtils.apiService();
@@ -112,7 +136,81 @@ public class ProfileSettingActivity extends BaseActivity {
         etAddress = (EditText) findViewById(R.id.et_profset_address);
 
         tvSave = (TextView) findViewById(R.id.tv_profset_save);
+        ivDp = (CircleImageView) findViewById(R.id.iv_dp);
 
+        ivDp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                takePicture();
+            }
+        });
+
+    }
+
+    public void takePicture()
+    {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                /*Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.dattabot.surveygisclick",
+                        photoFile);*/
+                Uri photoURI = Uri.fromFile(photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, ACTION_REQUEST_CAMERA);
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        imagePath = image.getAbsolutePath();
+        return image;
+    }
+
+    public void pickFromGallery()
+    {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode)
+        {
+            case ACTION_REQUEST_CAMERA:
+                if (resultCode == Activity.RESULT_OK)
+                {
+                    imagePath = mCurrentPhotoPath;
+                    File mediaFile = new File(imagePath);
+                    Glide.with(this).load(mediaFile).asBitmap().into(ivDp);
+
+                }
+                break;
+        }
     }
 
     public void saveProfileChange()
